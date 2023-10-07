6ff29ffae1411ee8718f104c5cfa8d62fcf1223b
BalogunVictor <balogunmoyinoluwav@gmail.com>
2023-10-07 18:32:45 +0100

    toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast';
 import type { AppProps } from 'next/app';
 import NextProgress from 'nextjs-progressbar';
 
@@ -10,6 +11,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextProgress color="#0d9488" options={{ showSpinner: false }} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          error: {
+            className: '!bg-rose-700 w-82 text-sm font-bold !text-white',
+          },
+          success: {
+            className: '!bg-lime-900 w-82 text-sm font-bold !text-white',
+          },
+        }}
+      />
       <Component {...pageProps} />
     </>
   );

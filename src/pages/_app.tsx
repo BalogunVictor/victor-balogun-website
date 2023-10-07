f044fa51c9ef1c8c338b7e5dc8bd79fa798d83ec
BalogunVictor <balogunmoyinoluwav@gmail.com>
2023-10-07 18:25:45 +0100

    progressive bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import NextProgress from 'nextjs-progressbar';
 
 // Import the styles provided by the react-pdf-viewer packages
 import '@/styles/globals.css';
@@ -6,5 +7,10 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextProgress color="#0d9488" options={{ showSpinner: false }} />
+      <Component {...pageProps} />
+    </>
+  );
 }

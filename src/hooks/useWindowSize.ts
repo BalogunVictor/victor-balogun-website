a2e9d2dd685408ed4790c993b801f8971ae2466f
BalogunVictor <balogunmoyinoluwav@gmail.com>
2023-10-08 08:57:36 +0100

    window size

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+  });
+
+  useEffect(() => {
+    function handleResize(): void {
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
